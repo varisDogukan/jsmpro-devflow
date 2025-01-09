ab14e9905fdcfb9dea99c30376fd2ae0d5fbf60e
dogukanVaris <varisdogukan@outlook.com>
2025-01-09 16:15:31 +0300

    Setup tailwind, eslint and prettier config

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <h1 className='text-3xl text-violet-700 font-black'>
+    <h1 className='text-3xl font-black text-violet-700'>
       Welcome to the world of Next.js
     </h1>
   );
