96cbce2e1ff38aaae1a958570749e4e3b6cc01e0
dogukanVaris <varisdogukan@outlook.com>
2025-01-11 10:17:25 +0300

    Added Font family

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <h1 className='text-3xl font-black text-violet-700'>
-      Welcome to the world of Next.js
-    </h1>
+    <>
+      <h1 className='h1-bold font-black text-violet-700'>
+        Welcome to the world of Next.js
+      </h1>
+    </>
   );
 }
