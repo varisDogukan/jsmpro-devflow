254ae9d097c53024bb57f606f4ac7fa3ee0db526
dogukanVaris <varisdogukan@outlook.com>
2025-01-18 16:07:54 +0300

    Implement LeftSidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import LeftSidebar from "@/components/navigation/LeftSidebar";
 import Navbar from "@/components/navigation/navbar";
 import React from "react";
 
@@ -7,10 +8,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main>
+    <main className='background-light850_dark100 relative'>
       <Navbar />
 
-      {children}
+      <div className='flex'>
+        <LeftSidebar />
+
+        <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:px-14 sm:px-14'>
+          <div className='mx-auto w-full max-w-5xl'>{children}</div>
+        </section>
+      </div>
     </main>
   );
 }
